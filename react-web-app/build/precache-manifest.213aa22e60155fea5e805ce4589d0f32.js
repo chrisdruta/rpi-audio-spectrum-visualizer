dbf6162e2161f5281ab859e9abed11435a4aeb9a
Chris Druta <chris.druta@gmail.com>
2020-09-18 21:56:39 -0700

    remove react build static files from gitignore

---
+++ react-web-app/build/precache-manifest.213aa22e60155fea5e805ce4589d0f32.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e00affd9f226464fc8d221176a6fdea1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "464e37250fa705a26bad",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6fceb457e4e8ccf6e318",
+    "url": "/static/js/2.10fa7c15.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.10fa7c15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "464e37250fa705a26bad",
+    "url": "/static/js/main.4148cdae.chunk.js"
+  },
+  {
+    "revision": "5499268e1720d9444bff",
+    "url": "/static/js/runtime-main.b7e8bbdf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
