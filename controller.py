63355375af6c9283d25a66bed93732e72599bf9b
Chris Druta <chris.druta@gmail.com>
2020-09-21 00:22:10 -0700

    fix indent

--- controller.py
+++ controller.py
@@ -93,27 +93,27 @@ class States(Enum):
         config = conpat % (BARS_NUMBER, RAW_TARGET, OUTPUT_BIT_FORMAT)
         bytetype, bytesize, bytenorm = ("H", 2, 65535) if OUTPUT_BIT_FORMAT == "16bit" else ("B", 1, 255)
 
-         with tempfile.NamedTemporaryFile() as config_file:
-        config_file.write(config.encode())
-        config_file.flush()
+        with tempfile.NamedTemporaryFile() as config_file:
+            config_file.write(config.encode())
+            config_file.flush()
 
-        process = subprocess.Popen(["cava", "-p", config_file.name], stdout=subprocess.PIPE)
-        chunk = bytesize * BARS_NUMBER
-        fmt = bytetype * BARS_NUMBER
+            process = subprocess.Popen(["cava", "-p", config_file.name], stdout=subprocess.PIPE)
+            chunk = bytesize * BARS_NUMBER
+            fmt = bytetype * BARS_NUMBER
 
-        if RAW_TARGET != "/dev/stdout":
-            if not os.path.exists(RAW_TARGET):
-                os.mkfifo(RAW_TARGET)
-            source = open(RAW_TARGET, "rb")
-        else:
-            source = process.stdout
+            if RAW_TARGET != "/dev/stdout":
+                if not os.path.exists(RAW_TARGET):
+                    os.mkfifo(RAW_TARGET)
+                source = open(RAW_TARGET, "rb")
+            else:
+                source = process.stdout
 
-        while state_machine.current_state == States.cava:
-            data = source.read(chunk)
-            if len(data) < chunk:
-                break
-            # sample = [i for i in struct.unpack(fmt, data)]  # raw values without norming
-            sample = [i / bytenorm for i in struct.unpack(fmt, data)]
-            print(sample)
+            while state_machine.current_state == States.cava:
+                data = source.read(chunk)
+                if len(data) < chunk:
+                    break
+                # sample = [i for i in struct.unpack(fmt, data)]  # raw values without norming
+                sample = [i / bytenorm for i in struct.unpack(fmt, data)]
+                print(sample)
 
         return
