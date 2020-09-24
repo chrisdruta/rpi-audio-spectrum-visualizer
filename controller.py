d9adbc9272c0a7bdf185f56e060add4a174d46c4
Chris Druta <chris.druta@gmail.com>
2020-09-24 00:04:09 -0700

    fine tune pink breath

--- controller.py
+++ controller.py
@@ -84,19 +84,19 @@ class States(Enum):
         return
 
     def pink(state_machine: StateMachine):
-        hue, sat, lum = (328/360, 100/100, 40/100)
+        hue, sat, lum = (328/360, 100/100, 21/100)
         while state_machine.current_state == States.pink:
             for i in range(40):
-                lum -= 0.01
+                lum -= 0.005
                 r, g, b = colorsys.hls_to_rgb(hue, lum, sat)
                 val = (int(r * 255), int(g * 255), int(b * 255))
 
                 state_machine.pixels.fill(val)
                 state_machine.pixels.show()
                 time.sleep(0.4)
-            time.sleep(1)
+            time.sleep(4)
             for i in range(40):
-                lum += 0.01
+                lum += 0.005
                 r, g, b = colorsys.hls_to_rgb(hue, lum, sat)
                 val = (int(r * 255), int(g * 255), int(b * 255))
 
