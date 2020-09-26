2570971bf41e2ccc95a5309ffe9adcfc745d4efb
Chris Druta <chris.druta@gmail.com>
2020-09-26 14:45:18 -0700

    show off

--- controller.py
+++ controller.py
@@ -30,7 +30,7 @@ class StateMachine:
         self.current_state = None
         self.wait = wait
 
-        self.num_pixels = 54
+        self.num_pixels = 64
         self.pixels = WS2801(board.SCLK, board.MOSI, self.num_pixels, auto_write=False, baudrate=1000000)
         self.pixels.fill((255, 0, 0))
         self.pixels.show()
@@ -209,7 +209,7 @@ class States:
                 # sample = [i for i in struct.unpack(fmt, data)]  # raw values without norming
                 sample = [i / bytenorm for i in struct.unpack(fmt, data)]
                 for i, level in enumerate(sample):
-                    val = get_bar_color(i, level, val=0.5, start_hue=300, end_hue=125)
+                    val = get_bar_color(i, level, val=0.5, start_hue=0, end_hue=360)
                     #val = sat_range(i, level, lum=0.25, hue=250, start_sat = 100, end_sat=0)
                     state_machine.pixels[i] = val
                     state_machine.pixels[state_machine.num_pixels - 1 - i] = val
