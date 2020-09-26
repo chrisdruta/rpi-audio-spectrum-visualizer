697587a2ae650ce4e07ae27b815045a68291c97b
Chris Druta <chris.druta@gmail.com>
2020-09-26 13:17:25 -0700

    try hsv

--- controller.py
+++ controller.py
@@ -158,9 +158,9 @@ class States:
         config = conpat % (BARS_NUMBER, RAW_TARGET, OUTPUT_BIT_FORMAT)
         bytetype, bytesize, bytenorm = ('H', 2, 65535) if OUTPUT_BIT_FORMAT == '16bit' else ('B', 1, 255)
 
-        def hue_range(i, level, lum=0.25, start_hue = 0, end_hue=360):
-            hue, sat, lum = (start_hue/360, 1.0, lum)
-            lum *= level
+        def hue_range(i, level, val=1.0, start_hue = 0, end_hue=360):
+            hue, sat, val = (start_hue/360, 1.0, val)
+            val *= level
 
             hue_delta = end_hue - start_hue
             if hue_delta < 0:
@@ -170,7 +170,7 @@ class States:
             if hue > 1:
                 hue -= 1
 
-            r, g, b = colorsys.hls_to_rgb(hue, lum, sat)
+            r, g, b = colorsys.hsv_to_rgb(hue, sat, val)
             return ((int(r * 255), int(g * 255), int(b * 255)))
 
         def sat_range(i, level, lum=0.25, hue=0, start_sat = 100, end_sat=0):
@@ -209,7 +209,7 @@ class States:
                 # sample = [i for i in struct.unpack(fmt, data)]  # raw values without norming
                 sample = [i / bytenorm for i in struct.unpack(fmt, data)]
                 for i, level in enumerate(sample):
-                    val = get_bar_color(i, level, lum=0.5, start_hue=0, end_hue=180)
+                    val = get_bar_color(i, level, val=1.0, start_hue=0, end_hue=180)
                     #val = sat_range(i, level, lum=0.25, hue=250, start_sat = 100, end_sat=0)
                     state_machine.pixels[i] = val
                     state_machine.pixels[state_machine.num_pixels - 1 - i] = val
