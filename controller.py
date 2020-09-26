445b29597c5bafae242cf4df3fa44db168426a8b
Chris Druta <chris.druta@gmail.com>
2020-09-26 16:44:44 -0700

    change hue range

--- controller.py
+++ controller.py
@@ -167,8 +167,7 @@ class States:
                 hue_delta += 360
 
             hue += i / BARS_NUMBER * hue_delta / 360
-            if hue > 1:
-                hue -= 1
+            hue -= int(hue)
 
             r, g, b = colorsys.hsv_to_rgb(hue, sat, val)
             return ((int(r * 255), int(g * 255), int(b * 255)))
@@ -209,7 +208,7 @@ class States:
                 # sample = [i for i in struct.unpack(fmt, data)]  # raw values without norming
                 sample = [i / bytenorm for i in struct.unpack(fmt, data)]
                 for i, level in enumerate(sample):
-                    val = get_bar_color(i, level, val=1.0, start_hue=0, end_hue=360)
+                    val = get_bar_color(i, level, val=1.0, start_hue=355, end_hue=360)
                     #val = sat_range(i, level, lum=0.25, hue=250, start_sat = 100, end_sat=0)
                     state_machine.pixels[i] = val
                     state_machine.pixels[state_machine.num_pixels - 1 - i] = val
