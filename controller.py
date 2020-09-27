47f34ca1b228aea344d030e5384275cf592953b7
Chris Druta <chris.druta@gmail.com>
2020-09-26 19:15:29 -0700

    bruh

--- controller.py
+++ controller.py
@@ -165,7 +165,7 @@ class States:
         bytetype, bytesize, bytenorm = ('H', 2, 65535) if OUTPUT_BIT_FORMAT == '16bit' else ('B', 1, 255)
 
         def hue_range(i, level, val=1.0, start_hue = 0, end_hue=360):
-            hue, sat, val = (start_hue/360, 1.0, val)
+            hue, sat, val = (start_hue/360, 0.9, val)
             val *= level
 
             hue_delta = end_hue - start_hue
@@ -214,7 +214,7 @@ class States:
                 # sample = [i for i in struct.unpack(fmt, data)]  # raw values without norming
                 sample = [i / bytenorm for i in struct.unpack(fmt, data)]
                 for i, level in enumerate(sample):
-                    val = get_bar_color(i, level, val=1.0, start_hue=240, end_hue=170)
+                    val = get_bar_color(i, level, val=1.0, start_hue=0, end_hue=360)
                     #val = sat_range(i, level, lum=0.25, hue=250, start_sat = 100, end_sat=0)
                     state_machine.pixels[i] = val
                     state_machine.pixels[state_machine.num_pixels - 1 - i] = val
